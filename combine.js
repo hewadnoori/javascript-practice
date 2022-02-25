ddb94b79e57dc4812e589ae1b21ec57366518110
Hewad Noori <hewadnoori16@gmail.com>
2022-02-25 14:34:23 -0500

    combined strings

---
+++ combine.js
@@ -0,0 +1,6 @@
+const name = prompt("What is your name?");
+let message = "Hello " + name + ". Welcome to my website! ";
+message += "I'm so happy that you came by to visit, " + name;
+message += ". Feel free to come again and listen to more music!"
+
+console.log(message);
