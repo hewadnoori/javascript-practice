9656fd9b8fc7500081e6dc47f9a31e1353c646d4
Hewad Noori <hewadnoori16@gmail.com>
2022-02-25 01:04:25 -0500

    beginning code

---
+++ javascript.js
@@ -0,0 +1,6 @@
+alert("Hello, world!");
+console.log("Hello from the console.");
+alert("Thanks for visiting");
+document.write("<h1>welcome to my web page.<h1>")
+var message = "Hello!";
+console.log(message);
