bf908ea2c60517a8da5b4406f52b6548e6df4a28
Hewad Noori <hewadnoori16@gmail.com>
2022-02-27 12:59:43 -0500

    converted string to number

---
+++ string-to-number.js
@@ -0,0 +1,10 @@
+const HTMLBadges = prompt('How many HTML badges do you have?');
+const CSSBadges = prompt('How many CSS badges do you have?');
+
+//You can use parseInt() to convert whole string numbers into an integer and parseFloat() for numbers like 3.14
+//const totalBadges = parseInt(HTMLBadges) + parseInt(CSSBadges); 
+
+//A MUCH BETTER WAY IS USING + which can convert to int or float
+const totalBadges = +HTMLBadges + +CSSBadges;
+
+console.log(totalBadges);
