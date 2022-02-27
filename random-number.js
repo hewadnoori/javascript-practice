7cce50e8708fcef1d8adc7f2c98b2e940618167c
Hewad Noori <hewadnoori16@gmail.com>
2022-02-27 18:36:26 -0500

    random number generator polished

--- random-number.js
+++ random-number.js
@@ -4,10 +4,11 @@
 //Math.ceil() rounds a number up
 //Math.floor(Math.random() * 6) + 1 random number between 1 - 6
 
-//ask user for a number that will determine the upper limit of the random number generator
-let userInp = prompt("give me a number");
-//users input is used to make the random number generator which will go from 1 to userinp and is rounded down. the user input is also turned into a integer.
-let random = Math.floor((Math.random() * +userInp) + 1)
+//ask user for a number that will determine the upper and lower limit of the random number generator
+let lowInp = prompt("give me your lowest number");
+let highInp = prompt("give me your highest number");
+//users input is used to make the random number generator which is rounded down. the user input is also turned into a integer.
+let random = Math.floor((Math.random() * (+highInp - +lowInp)) + +lowInp)
 //the results are printed to the console.
-console.log(`${random} is a random number between 1 and ${userInp}`)
+console.log(`${random} is a random number between ${lowInp} and ${highInp}`)
 
