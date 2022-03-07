e6c1fd4079e4f39230bb968b11cc6fb68cdbc3e2
Hewad Noori <hewadnoori16@gmail.com>
2022-03-07 01:11:25 -0500

    made array quiz more complex

--- quiz-using-arrays.js
+++ quiz-using-arrays.js
@@ -4,7 +4,8 @@ const quiz = [
     ["What is the best food on earth?", "pizza"],
     ["What continent is the U.S. on?", "north america"],
 ];
-
+let wrong = "";
+let right = "";
 // 2. Store the number of questions answered correctly
 let correct = 0;
 
@@ -23,7 +24,19 @@ for (let i = 0; i < quiz.length; i++) {
     }
     else if (quiz[i].includes(input.toLowerCase())) {
         correct += 1;
+        right += `<li>${quiz[i][0]}</li>`
+    }
+    else {
+        wrong += `<li>${quiz[i][0]}</li>`
     }
 }
 // 4. Display the number of correct answers to the user
-document.querySelector('main').innerHTML = `You have answered ${correct} correct!`
+document.querySelector('main').innerHTML = `<h3>You got these questions right:</h3>
+<ol>
+${right}
+</ol>
+<h3>You got these questions wrong:</h3>
+<ol>
+${wrong}
+</ol>
+`
