d56ce36a27f8ce5eb4f56551f0c8d9dd87da7d74
Hewad Noori <hewadnoori16@gmail.com>
2022-03-18 16:36:38 -0400

    added dom notes

--- Interacting with the DOM/app.js
+++ Interacting with the DOM/app.js
@@ -2,13 +2,30 @@ const btnCreate = document.getElementById('btn-main');
 const btnToggle = document.querySelector('.btn-toggle');
 const btnRemove = document.querySelector('.btn-remove');
 const taskList = document.querySelector('.list-container ul');
+const listItems = taskList.children;
 
 
-taskList.addEventListener('mouseover', (event) => {
-    if (event.target.tagName === 'LI') {
-        event.target.textContent = event.target.textContent.toUpperCase();
+function attachRemoveButton(li) {
+    let remove = document.createElement('button')
+    remove.className = 'remove';
+    remove.textContent = 'Remove';
+    li.appendChild(remove);
+}
+for (let i = 0; i < listItems.length; i++) {
+    attachRemoveButton(listItems[i]);
+}
+
+document.addEventListener('click', (event) => {
+    console.log(event.target);
+})
+taskList.addEventListener('click', (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        const button = event.target;
+        const li = button.parentNode;
+        li.remove();
     }
 })
+
 
 btnToggle.addEventListener('click', () => {
     const listContainer = document.querySelector('.list-container');
@@ -22,16 +39,12 @@ btnToggle.addEventListener('click', () => {
 });
 
 btnCreate.addEventListener('click', () => {
+    let ul = document.getElementsByTagName('ul')[0];
     const input = document.querySelector('.input-main');
-    const list = document.querySelector('ul');
-    list.insertAdjacentHTML(
-        'afterbegin',
-        `<li>${input.value}</i>`
-    );
+    let li = document.createElement('li');
+    li.textContent = input.value;
+    attachRemoveButton(li);
+    ul.appendChild(li);
     input.value = '';
 });
 
-btnRemove.addEventListener('click', () => {
-    const li = document.querySelector('li:last-child');
-    li.remove();
-});
--- Interacting with the DOM/temp.js
+++ Interacting with the DOM/temp.js
@@ -1,4 +1,13 @@
 function sayHi() {
     console.log("Hello")
 }
-sayHi();
+function hiAndBye(func) {
+    func()
+    console.log('Bye')
+}
+hiAndBye(sayHi);
+
+//you can also run the function directly into hiAndBye
+//hiAndBye(() => { //this runs an anonymous function into a function
+//console.log("Greeting");
+//})
