881d13246ebc0a8f320b377518288483f740c449
Hewad Noori <hewadnoori16@gmail.com>
2022-03-23 20:36:28 -0400

    finished dom practice

--- Interacting with the DOM/Traversing-the-DOM/js/script.js
+++ Interacting with the DOM/Traversing-the-DOM/js/script.js
@@ -2,31 +2,31 @@
 const list = document.querySelector('.list');
 
 // 1: Store the first child of the `ul` in the variable `firstItem`
-const firstItem = list.firstChild('li'); console.log(firstItem);
+const firstItem = list.firstElementChild; console.log(firstItem);
 firstItem.style.backgroundColor = '#04c5e6';
 
 // 2: Using traversal, store the second list item in a variable named `nextItem`
-const nextItem = firstItem.nextSibling('li'); console.log(nextItem);
+const nextItem = firstItem.nextElementSibling; console.log(nextItem);
 nextItem.style.backgroundColor = '#b7c7d0';
 
 // 3: Store the last child of the `ul` in a variable named `lastItem`
-
+const lastItem = list.lastElementChild;
 lastItem.style.backgroundColor = '#57d6ab';
 
 // 4: Using traversal, store the second-to-last list item in a variable named `prevItem`
-
+const prevItem = lastItem.previousElementSibling;
 prevItem.style.backgroundColor = '#f36f49';
 
 // 5: Store the nested div in a variable named `banner`
-
+const banner = list.previousElementSibling;
 banner.className = 'banner';
 
 // 6: Using traversal, store the wrapper div in a variable named `wrapper`
-
+const wrapper = list.parentElement; console.log(wrapper);
 wrapper.style.backgroundColor = '#fcfcfc';
 
 // 7: Using traversal, store the body in a variable named `body`
-
+const body = wrapper.parentElement;
 body.style.backgroundColor = '#f8fdf3';
 
 
