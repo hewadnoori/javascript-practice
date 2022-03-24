329bfacbd9db37b092842b9b44f90a279e1d5f14
Hewad Noori <hewadnoori16@gmail.com>
2022-03-23 20:12:41 -0400

    added more notes and cleaned up file

--- Interacting with the DOM/practice-DOM-manipulation/js/scripts.js
+++ Interacting with the DOM/practice-DOM-manipulation/js/scripts.js
@@ -1,9 +1,6 @@
 // 1: Set the text of the <h1> element
 const title = document.querySelector('h1');
-title.insertAdjacentHTML(
-    `afterbegin`,
-    `DOM Manipulation Practice`
-);
+title.textContent = "My Activities List";
 // 2: Set the color of the <h1> to a different color
 
 title.style.color = "red";
@@ -13,23 +10,39 @@ title.style.color = "red";
 const desc = document.querySelector('.desc');
 desc.insertAdjacentHTML(
     'afterbegin',
-    `This is my practice for <strong>DOM manipulation</strong> `
+    `A list of <em>fun</em> things I want to do today`
 );
+//or you can do desc.innerHTML = "A list of <em>fun</em> things I want to do today"
 
 // 4: Set the class of the <ul> to 'list'
 const list = document.querySelector('ul');
 list.className = 'list';
 // 5: Create a new list item and add it to the <ul>
-list.insertAdjacentHTML(
-    'beforeend',
-    `<li><input> Play league of legends</li>`
-);
+const item = document.createElement('li');
+item.innerHTML = "<input> Eat ice cream";
+list.appendChild(item);
+// list.insertAdjacentHTML(
+//     'beforeend',
+//     `<li><input> Play league of legends</li>`
+// );
 
 // 6: Change all <input> elements from text fields to checkboxes
-const inToCheck = document.querySelectorAll('li input'); console.log(inToCheck);
-
+const inToCheck = document.getElementsByTagName('input');;
+for (let i = 0; i < inToCheck.length; i++) {
+    inToCheck[i].type = 'checkbox';
+}
 // 7: Create a <button> element, and set its text to 'Delete'
 // Add the <button> inside the '.extra' <div>
-
+const deleteButton = document.createElement('button');
+deleteButton.textContent = "Delete";
+const extra = document.querySelector('.extra');
+//if you want to delete the text in the extra div
+//extra.textContent = "";
+extra.appendChild(deleteButton);
 
 // 8: Remove the '.extra' <div> element from the DOM when a user clicks the 'Delete' button
+const container = document.querySelector('.container');
+container.addEventListener('click', () => {
+    container.removeChild(extra);
+    //or extra.remove();
+})
