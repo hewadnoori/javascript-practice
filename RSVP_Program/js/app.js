75ee95587f04fa8df0c21614268200d0d9de9b32
Hewad Noori <hewadnoori16@gmail.com>
2022-03-25 19:13:11 -0400

    rsvp program almost done

--- RSVP_Program/js/app.js
+++ RSVP_Program/js/app.js
@@ -1,10 +1,44 @@
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+
+const mainDiv = document.querySelector('.main');
 const ul = document.getElementById('invitedList');
+
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckBox = document.createElement('input');
+
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckBox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckBox);
+mainDiv.insertBefore(div, ul);
+filterCheckBox.addEventListener('change', (e) => {
+    const isChecked = e.target.checked;
+    const lis = ul.children;
+    if (isChecked) {
+        for (let i = 0; i < lis.length; i++) {
+            let li = lis[i];
+            if (li.className === 'responded') {
+                li.style.display = '';
+            } else {
+                li.style.display = 'none';
+            }
+        }
+    } else {
+        for (let i = 0; i < lis.length; i++) {
+            let li = lis[i];
+            li.style.display = '';
+        }
+
+    }
+})
 
 function createLI(text) {
     const li = document.createElement('li');
-    li.textContent = text;
+    const span = document.createElement('span');
+    span.textContent = text;
+    li.appendChild(span);
     const label = document.createElement('label');
     label.textContent = 'Confirmed';
     const checkbox = document.createElement('input');
@@ -12,9 +46,13 @@ function createLI(text) {
     label.appendChild(checkbox);
     li.appendChild(label);
 
-    const button = document.createElement('button');
-    button.textContent = 'remove';
-    li.appendChild(button);
+    const editButton = document.createElement('button');
+    editButton.textContent = 'edit';
+    li.appendChild(editButton);
+
+    const removeButton = document.createElement('button');
+    removeButton.textContent = 'remove';
+    li.appendChild(removeButton);
     return li;
 }
 
@@ -39,8 +77,28 @@ ul.addEventListener('change', (e) => {
 });
 ul.addEventListener('click', (e) => {
     if (e.target.tagName === 'BUTTON') {
-        const li = e.target.parentNode;
+        const button = e.target;
+        const li = button.parentNode;
         const ul = li.parentNode;
-        ul.removeChild(li);
+        if (button.textContent === 'remove') {
+            ul.removeChild(li);
+        } else if (button.textContent === 'edit') {
+            const span = li.firstElementChild;
+            const input = document.createElement('input');
+            input.type = 'text';
+            input.value = span.textContent;
+            li.insertBefore(input, span);
+            li.removeChild(span);
+            button.textContent = 'save';
+        }
+        else if (button.textContent === 'save') {
+            const input = li.firstElementChild;
+            const span = document.createElement('span');
+            span.textContent = input.value;
+            input.value = span.textContent;
+            li.insertBefore(span, input);
+            li.removeChild(input);
+            button.textContent = 'edit';
+        }
     }
 });
