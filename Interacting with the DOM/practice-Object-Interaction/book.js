a60fddb42386e053346d4de58a706f730d3b1fca
Hewad Noori <hewadnoori16@gmail.com>
2022-04-23 07:55:23 -0400

    done with code

--- Interacting with the DOM/practice-Object-Interaction/book.js
+++ Interacting with the DOM/practice-Object-Interaction/book.js
@@ -3,5 +3,23 @@ class Book {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
+        this.patron = null;
+        this.dueDate = null;
+        this._out = false;
     }
+    get out() {
+        return this._out;
+    }
+    set out(out) {
+        this._out = out;
+        if (out) {
+            const newDueDate = new Date();
+            newDueDate.setDate(newDueDate.getDate() + 14);
+            this.dueDate = newDueDate;
+        }
+        else {
+            this.dueDate = null;
+        }
+    }
+
 }
