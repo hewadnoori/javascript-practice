471567869a3acaa419a914dbc801d6f8366074be
Hewad Noori <hewadnoori16@gmail.com>
2022-04-25 12:34:41 -0400

    finished callback functions

--- Interacting with the DOM/callback-functions/callbacks-on-text-inputs/app.js
+++ Interacting with the DOM/callback-functions/callbacks-on-text-inputs/app.js
@@ -1,2 +1,19 @@
 const nameInput = document.getElementById('name');
 const messageTextArea = document.getElementById('message');
+
+const focusHandler = event => {
+    //target is the element that triggered the event.
+    event.target.className = 'highlight';
+};
+//to remove the highlight when it isnt being focused.
+const blurHandler = event => {
+    event.target.className = '';
+};
+
+nameInput.addEventListener('focus', focusHandler);
+
+nameInput.addEventListener('blur', blurHandler);
+
+messageTextArea.addEventListener('focus', focusHandler);
+
+messageTextArea.addEventListener('blur', blurHandler);
