10bcdd00631e0f49acf827c629b151fbce9cd777
Hewad Noori <hewadnoori16@gmail.com>
2023-05-13 02:09:24 -0400

    added prime

---
+++ Java/prime.js
@@ -0,0 +1,16 @@
+function isPrime(num){
+  for(let i = 2; i <= num; i++){
+    if(primeCheck(i)){
+      console.log(i);
+    }
+  }
+}
+function primeCheck(num){
+  for(let i = 2; i < num; i++){
+    if(num % i === 0){
+      return false;
+    }
+  }
+  return num > 1;
+}
+isPrime(100);
